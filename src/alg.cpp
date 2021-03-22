052979686092d623b4474f27e251b68df3a78563
Мартынова Ирина <martinova.iri@yandex.ru>
2021-03-22 17:55:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+   int i = 0, j = size - 1, count = 0;
+    while (i < j) {
+      int	mid = i + (j-i) / 2;
+        if (arr[mid] < value)
+            i = mid + 1;
+        else
+          j = mid;
+    }
+    if (arr[i] == value) {
+        while (arr[i] == value) {
+            count++;
+            i++;
+        }
+    }
+        if (count != 0)
+            return count;
+        else
+            return 0;
 }
