887f1e3dfac5d7b47d57fdccdc978885bfab0bef
Мартынова Ирина <martinova.iri@yandex.ru>
2021-03-22 18:06:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 int cbinsearch(int *arr, int size, int value) {
   int i = 0, j = size - 1, count = 0;
     while (i < j) {
-       int	mid = i + (j-i) / 2;
+       int mid = i + (j-i) / 2;
         if (arr[mid] < value)
             i = mid + 1;
         else
